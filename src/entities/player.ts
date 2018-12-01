f44bd775b0d116a6ed1fbde97d5bff937cff689a
Tommi Sinivuo <tommi.sinivuo@wunderdog.fi>
2018-12-01 15:08:57 +0200

    Ability to disable double jump

--- src/entities/player.ts
+++ src/entities/player.ts
@@ -5,9 +5,10 @@ export class Player {
   physicsImage: Physics.Arcade.Image;
   cursors: Input.Keyboard.CursorKeys;
   isJumping: boolean = false;
+  doubleJump = true;
 
   constructor(private scene: GameScene) {
-    this.physicsImage = scene.physics.add.image(200, 500, 'player');
+    this.physicsImage = scene.physics.add.image(100, 100, 'player');
     scene.add.existing(this.physicsImage);
     this.cursors = scene.input.keyboard.createCursorKeys();
     this.physicsImage.setPipeline('Light2D');
@@ -22,10 +23,18 @@ export class Player {
       this.physicsImage.setVelocityX(0);
     }
 
-    if (Input.Keyboard.JustDown(this.cursors.up!) && (this.isOnFloor || this.isJumping)) {
+    if (Input.Keyboard.JustDown(this.cursors.up!) && (this.body.onFloor() || this.canDoubleJump)) {
       this.physicsImage.setVelocityY(-200);
       this.isJumping = !this.isJumping;
     }
+  }
+
+  get canDoubleJump() {
+    return this.doubleJump && this.isJumping
+  }
+
+  get body(): Physics.Arcade.Body {
+    return this.physicsImage.body as any
   }
 
   get isOnFloor(): boolean {
