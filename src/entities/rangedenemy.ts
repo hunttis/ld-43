d6856da72c666168ee83118e7031c014442c3a38
Tommi Sinivuo <tommi.sinivuo@wunderdog.fi>
2018-12-02 12:36:45 +0200

    Enemy bows v2

--- src/entities/rangedenemy.ts
+++ src/entities/rangedenemy.ts
@@ -4,13 +4,17 @@ import { EnemyBullet } from './enemybullet';
 import { Enemy } from './enemy';
 
 export class RangedEnemy extends Enemy {
-  WEAPON_COOLDOWN_MAX: number = 2500;
+  WEAPON_COOLDOWN_MAX: number = Phaser.Math.Between(1500, 3000);
+  SHOOT_DELAY_MAX: number = 500;
 
   bowSprite!: GameObjects.Sprite;
 
   seesPlayer: boolean = false;
   weaponCooldown: number = 0;
-  facing!: integer;
+  facing: integer = -1;
+  isFiring: boolean = false;
+  hasFired: boolean = false;
+  shootDelay = 0;
 
   constructor(scene: GameScene, x: number, y: number) {
     super(scene, x, y, 'player');
@@ -19,6 +23,8 @@ export class RangedEnemy extends Enemy {
     this.setPipeline('Light2D');
 
     this.bowSprite = new GameObjects.Sprite(scene, this.body.x, this.body.y, 'bow');
+    this.bowSprite.setAngle(45 * this.facing);
+    this.bowSprite.setPosition(this.body.x + 16 + (this.facing * 8), this.body.y + 12);
     this.scene.add.existing(this.bowSprite);
   }
 
@@ -30,7 +36,7 @@ export class RangedEnemy extends Enemy {
     if (this.weaponCooldown <= this.WEAPON_COOLDOWN_MAX) {
       this.weaponCooldown += this.scene.sys.game.loop.delta;
     }
-    if (distanceToPlayer < 1000) {
+    if (distanceToPlayer < 500) {
       if (this.weaponCooldown > this.WEAPON_COOLDOWN_MAX) {
         this.shoot();
         this.weaponCooldown = 0;
@@ -41,13 +47,41 @@ export class RangedEnemy extends Enemy {
 
   updateBow() {
     this.bowSprite.scaleX = this.facing;
-    this.bowSprite.setPosition(this.body.x + 16 + (this.facing * 8), this.body.y + 12);
+    //this.bowSprite.setX(this.body.x + 16 + (this.facing * 8));
+
+    if (this.hasFired) {
+      this.hasFired = false;
+      this.scene.add.tween({
+        targets: this.bowSprite,
+        duration: 200,
+        angle: 45 * this.facing,
+        x: this.body.x + 16 + (this.facing * 6),
+        y: this.body.y + 24
+      });
+    }
+
+    if (this.isFiring) {
+      this.shootDelay += this.scene.sys.game.loop.delta;
+      if (this.shootDelay > this.SHOOT_DELAY_MAX) {
+        this.shootDelay = 0;
+        this.isFiring = false;
+        this.hasFired = true;
+        const bullet = new EnemyBullet(this.scene, this.x, this.y, this.facing);
+        this.scene.add.existing(bullet);
+        this.scene.enemyBullets.add(bullet);
+      }
+    }
   }
 
   shoot() {
-    const bullet = new EnemyBullet(this.scene, this.x, this.y, this.facing);
-    this.scene.add.existing(bullet);
-    this.scene.enemyBullets.add(bullet);
+    this.isFiring = true;
+    this.scene.add.tween({
+      targets: this.bowSprite,
+      duration: 100,
+      angle: 0,
+      x: this.body.x + 16 + (this.facing * 12),
+      y: this.body.y + 8
+    });
   }
 
 }
