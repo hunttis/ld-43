0f05842884a1d8666fde8eb09c4dfefe69bd702f
Ville Nuutinen <ville.nuutinen@wunder.dog>
2018-12-02 16:51:05 +0200

    Sign depth fix

--- src/entities/sign.ts
+++ src/entities/sign.ts
@@ -45,6 +45,7 @@ export class Sign extends Physics.Arcade.Sprite {
     this.textContent.y = this.signLocationY + this.signHeight / 2;
     this.textContent.setScrollFactor(0);
 
+    this.textBox.depth = 120;
     this.textContent.depth = 120;
 
     this.textBox.visible = false;
