99d1971b15f73b4c496e2264407da2ad078b4316
Ville Nuutinen <ville.nuutinen@wunder.dog>
2018-12-02 15:35:20 +0200

    Arrow depth

--- src/entities/bullet.ts
+++ src/entities/bullet.ts
@@ -17,7 +17,7 @@ export class Bullet extends PlayerAttack {
     this.setVelocityX(direction * 300.0);
     this.body.setAllowGravity(false);
     this.scaleX = direction;
-    this.depth = 102;
+    this.depth = 91;
   }
 
   update() {
